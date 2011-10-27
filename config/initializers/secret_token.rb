8061d440b3ef911cc4ec1c181d566c9fb354a09f
Graeme Mathieson <mathie@woss.name>
2011-10-27 14:56:39 +0100

    rails new known-buggers -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KnownBuggers::Application.config.secret_token = '342e62c3bec630dd937b88cb050589d38a6f5ce76ff57ef6282e44c74fb6922666cd98dabd411bc6132373d31c5a758bdbcbbee919db9ec92c6be0e582adf58a'
